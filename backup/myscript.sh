e1f0a08d605c11e4773f07ebe32c979eae3e6dbc
Immanuel <79416384+Hzz-Hzz@users.noreply.github.com>
2021-09-26 11:50:01 +0700

    26/09/2021 11:50:01

---
+++ backup/myscript.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# REV00 Sun 12 Sep 2021 03:10:00 WIB
+# START Sun 12 Sep 2021 03:10:00 WIB
+
+FILES="my*.txt my*.sh"
+SHA="SHA256SUM"
+
+# echo "rm -f $SHA $SHA.asc"
+rm -f $SHA $SHA.asc
+
+# echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo ''
+echo ''
+
+# echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo ''
+echo ''
+
+# echo "gpg --output $SHA.asc --armor --sign --detach-sign $SHA"
+gpg --output $SHA.asc --armor --sign --detach-sign $SHA
+
+echo ''
+echo ''
+
+# echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
